459a6088eacdfd4ee1a0ccd11417eeb87085cb7f
yzengal <yzengal@connect.ust.hk>
2024-09-05 01:34:35 +0800

    the socket client/server has been tested

--- lecture2_RPC/socket/server.cpp
+++ lecture2_RPC/socket/server.cpp
@@ -31,7 +31,7 @@ int main() {
         exit(EXIT_FAILURE);  
     }  
   
-    // Forcefully attaching socket to the port 1234  
+    // Forcefully attaching socket to the port 5000  
     address.sin_family = AF_INET;  
     address.sin_addr.s_addr = INADDR_ANY;  
     address.sin_port = htons(PORT);  
