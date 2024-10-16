670d19db9c293cc3084610dcb162cb0ffb004a93
yzengal <yzengal@connect.ust.hk>
2024-10-16 09:53:31 +0800

    slightlt change enviroment.sh by including SEAL and Boost

--- Lecture4_HE/asymmetric_psa/environment.sh
+++ Lecture4_HE/asymmetric_psa/environment.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export MY_GRPC_PATH="/opt/gRPC/"
+export MY_BOOST_PATH="/opt/boost/"
+export MY_SEAL_PATH="/opt/SEAL/"
 export PATH="${MY_GRPC_PATH}/bin:$PATH"
-export LD_LIBRARY_PATH="${MY_GRPC_PATH}/lib:$LD_LIBRARY_PATH"
-export C_INCLUDE_PATH="${MY_GRPC_PATH}/include:$C_INCLUDE_PATH"
-export CPLUS_INCLUDE_PATH="${MY_GRPC_PATH}/include:$CPLUS_INCLUDE_PATH"
+export LD_LIBRARY_PATH="${MY_GRPC_PATH}/lib:${MY_BOOST_PATH}/lib::${MY_SEAL_PATH}/lib:$LD_LIBRARY_PATH"
+export C_INCLUDE_PATH="${MY_GRPC_PATH}/include:${MY_BOOST_PATH}/include:${MY_SEAL_PATH}/include:$C_INCLUDE_PATH"
+export CPLUS_INCLUDE_PATH="${MY_GRPC_PATH}/include:${MY_BOOST_PATH}/include:${MY_SEAL_PATH}/include:$CPLUS_INCLUDE_PATH"
